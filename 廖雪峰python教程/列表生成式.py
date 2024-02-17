4fa39be85b18213184517e8b39f6845ac4d51a97
summer506hai <1170618497@qq.com>
2024-02-17 16:57:19 +0800

    Create 列表生成式.py
    提交代码

---
+++ 廖雪峰python教程/列表生成式.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+转成小写
+'''
+L1 = ['Hello', 'World', 18, 'Apple', None]
+
+#使用列表生成式
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
+assert L2 == ['hello', 'world', 'apple']
+print("测试通过！")
+
+#使用map/filter
+def is_str(s):
+    return isinstance(s, str)
+
+def lower(s):
+    return s.lower()
+
+L2 = list(map(lower,list(filter(is_str,L1))))
+assert L2 == ['hello', 'world', 'apple']
+print("测试通过！")
